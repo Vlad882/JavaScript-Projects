a7c9bc098ed4cc219e0d34640d36975d7415e840
Vlad882 <zolotarev1982@gmail.com>
2023-12-22 21:13:05 +0000

    pusshing assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert('Hey Hey');   //Window.alert method
+
+document.write('Hey Hey Again');  //document.write method
+
+var Sent1 = 'This is the beginning of the string';  //assigning variable 1
+var Sent2 = 'and this is the end of the string';    // assigning variable 2
+document.write(Sent1 + Sent2);  // Concatenating variables
+
+document.write(5+5);  // Expression
